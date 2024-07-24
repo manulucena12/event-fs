29431aa96a1b246e6bbdf3b4b99a1654d8fba8ab
manulucena <mlucenasalas@gmail.com>
2024-07-24 16:23:36 +0200

    Ignoring build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import { eventsRouter } from "./routes/events";
 export const app = express();
 const { MONGO_DB_URI, MONGO_DB_URI_TEST, MONGO_DB_URI_DEV, NODE_ENV } =
   process.env;
-const uri =
+const uri: string =
   NODE_ENV === "test"
     ? MONGO_DB_URI_TEST
     : NODE_ENV === "dev"
