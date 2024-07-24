db4a6c6db6d5bbac670d791df5cd9a2c7b86d1f2
manulucena <mlucenasalas@gmail.com>
2024-07-24 16:52:05 +0200

    Fixing pipeline

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,12 @@ import { eventsRouter } from "./routes/events";
 export const app = express();
 const { MONGO_DB_URI, MONGO_DB_URI_TEST, MONGO_DB_URI_DEV, NODE_ENV } =
   process.env;
+
+if (!MONGO_DB_URI && !MONGO_DB_URI_TEST && !MONGO_DB_URI_DEV) {
+  throw new Error(
+    "At least one MongoDB URI must be defined in environment variables",
+  );
+}
 const uri: string =
   NODE_ENV === "test"
     ? MONGO_DB_URI_TEST
@@ -15,9 +21,14 @@ const uri: string =
 
 app.use(express.json());
 
-mongoose.connect(uri).then(() => {
-  console.log(`Mongo DB connected successfully on mode ${NODE_ENV}`);
-});
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log(`Mongo DB connected successfully on mode ${NODE_ENV}`);
+  })
+  .catch((err) => {
+    console.error(`Mongo DB connection error: ${err}`);
+  });
 
 app.use("/api/events", eventsRouter);
 
